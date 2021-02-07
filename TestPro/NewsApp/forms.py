a322ae9efafe67d0e49c5f6c5c26ddd74dae9fc1
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2021-02-07 14:29:17 +0530

    Add files via upload

--- TestPro/NewsApp/forms.py
+++ TestPro/NewsApp/forms.py
@@ -1,8 +1,60 @@
 from django import forms
+from .models import RegistrationData
 
 
 class RegistrationForms(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
-    email = forms.CharField(max_length=100)
-    phone = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=100,
+                               widget=forms.TextInput(attrs={
+                                   'class': 'form-control',
+                                   'placeholder': 'Username'
+                               }))
+    password = forms.CharField(max_length=100,
+                               widget=forms.PasswordInput(attrs={
+                                   'class': 'form-control',
+                                   'placeholder': 'Password'
+                               })
+                               )
+    email = forms.CharField(max_length=100,
+                            widget=forms.EmailInput(attrs={
+                                'class': 'form-control',
+                                'placeholder': 'Email'
+                            })
+                            )
+    phone = forms.CharField(max_length=100,
+                            widget=forms.NumberInput(attrs={
+                                'class': 'form-control',
+                                'placeholder': 'Phone Number'
+                            })
+                            )
+
+
+class RegistrationModal(forms.ModelForm):
+    class Meta:
+        model = RegistrationData
+
+        fields = [
+            'username',
+            'password',
+            'email',
+            'phone'
+        ]
+
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Username'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Password'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Email'
+            }),
+            'phone': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Phone Number'
+            })
+
+        }
